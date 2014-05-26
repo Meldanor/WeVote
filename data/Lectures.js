bdc9e8b8f55429677bca3d06b14df6f4c906025d
Kilian Gärtner <Bhaals@gmx.de>
2014-05-26 21:04:21 +0200

    Add sample lectures

---
+++ data/Lectures.js
@@ -0,0 +1,23 @@
+Lectures = [
+    {
+        key: '1. Semester',
+        items: [
+            { name: "EinfInf", voted: 20, possible: 30 },
+            { name: "TI I", voted: 10, possible: 13 },
+            { name: "Logik", voted: 25, possible: 33 },
+            { name: "SchlüKo", voted: 0, possible: 0 },
+            { name: "Mathe I", voted: 80, possible: 100 },
+        ]
+    },
+    {
+        key: '2. Semester',
+        items: [
+            { name: "AuD", voted: 13, possible: 16 },
+            { name: "PGP", voted: 0, possible: 0 },
+            { name: "Modellierung", voted: 1, possible: 2 },
+            { name: "Mathe II", voted: 22, possible: 33 },
+            { name: "SchlüKo II", voted: 0, possible: 0 },
+            { name: "TrainSMK", voted: 0, possible: 0 },
+        ]
+    }
+];
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 ﻿(function() {
     "use strict";
 
-    var MyApp = window.MyApp = { };
+    var WeVote = window.WeVote = { };
     
     // Uncomment the line below to disable platform-specific look and feel and to use the Generic theme for all devices
     // DevExpress.devices.current({ platform: "generic" });
 
     $(function() {
-        MyApp.app = new DevExpress.framework.html.HtmlApplication({
-            namespace: MyApp,
+        WeVote.app = new DevExpress.framework.html.HtmlApplication({
+            namespace: WeVote,
             
             navigationType: "navbar",
             navigation: [
@@ -30,8 +30,8 @@
             ]
         });
         
-        MyApp.app.router.register(":view", { view: "overview" });
-        MyApp.app.navigate();
+        WeVote.app.router.register(":view", { view: "overview" });
+        WeVote.app.navigate();
     });
     
 })();
