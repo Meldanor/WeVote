b7fb239649a48da16206e56591bac7de8a6d028d
Kilian Gärtner <Bhaals@gmx.de>
2014-05-28 00:12:05 +0200

    Add login placeholder

--- views/settings.js
+++ views/settings.js
@@ -1,1 +1,11 @@
 ﻿var askingFeedback = ko.observable(false);
+
+popupTitle = "My popup";
+buttonVisible = ko.observable(true);
+popupVisible = ko.observable(false);
+showPopup = function () {
+  popupVisible(true);
+};
+hidePopup = function () {
+  popupVisible(false);
+};
