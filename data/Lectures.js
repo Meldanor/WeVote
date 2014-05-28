0e7045aa6245fd59d3571a5f30f4db0db813cf9a
Kilian Gaertner <Bhaals@gmx.de>
2014-05-28 12:04:22 +0200

    Merge overview with calendar

--- data/Lectures.js
+++ data/Lectures.js
@@ -2,22 +2,22 @@ Lectures = [
     {
         key: '1. Semester',
         items: [
-            { name: "EinfInf", voted: 20, possible: 30 },
-            { name: "TI I", voted: 10, possible: 13 },
-            { name: "Logik", voted: 25, possible: 33 },
-            { name: "SchlüKo", voted: 0, possible: 0 },
-            { name: "Mathe I", voted: 80, possible: 100 },
+            { name: "EinfInf", voted: 20, possible: 30, day: 'Montag', time: '11:00', room: 'g29-336' },
+            { name: "TI I", voted: 10, possible: 13, day: 'Dienstag', time: '13:00', room: 'g29-114' },
+            { name: "Logik", voted: 25, possible: 33, day: 'Mittwoch', time: '13:00', room: 'g29-036' },
+            { name: "SchlüKo", voted: 0, possible: 0, day: 'Donnerstag', time: '13:00', room: 'g29-K36'  },
+            { name: "Mathe I", voted: 80, possible: 100, day: 'Freitag',time: '13:00',  room: 'g29-336'  }
         ]
     },
     {
         key: '2. Semester',
         items: [
-            { name: "AuD", voted: 13, possible: 16 },
-            { name: "PGP", voted: 0, possible: 0 },
-            { name: "Modellierung", voted: 1, possible: 2 },
-            { name: "Mathe II", voted: 22, possible: 33 },
-            { name: "SchlüKo II", voted: 0, possible: 0 },
-            { name: "TrainSMK", voted: 0, possible: 0 },
+            { name: "AuD", voted: 13, possible: 16, day: 'Montag', time: '11:00', room: 'g29-336' },
+            { name: "PGP", voted: 0, possible: 0, day: 'Dienstag', time: '13:00', room: 'g29-114' },
+            { name: "Modellierung", voted: 1, possible: 2,  day: 'Freitag',time: '13:00',  room: 'g29-336'  },
+            { name: "Mathe II", voted: 22, possible: 33,  day: 'Freitag',time: '13:00',  room: 'g29-336'  },
+            { name: "SchlüKo II", voted: 0, possible: 0,  day: 'Freitag',time: '13:00',  room: 'g29-336'  },
+            { name: "TrainSMK", voted: 0, possible: 0,  day: 'Freitag',time: '13:00',  room: 'g29-336'  }
         ]
     }
 ];
