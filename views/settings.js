8545c06500f8b6cfd3b23f4af25e515611f25e0a
Kilian Gärtner <Bhaals@gmx.de>
2014-05-29 20:57:37 +0200

    Save settings to useable variables

--- views/settings.js
+++ views/settings.js
@@ -1,11 +1,34 @@
-﻿var askingFeedback = ko.observable(false);
+﻿WeVote.settings = function (params) {
 
-popupTitle = "My popup";
-buttonVisible = ko.observable(true);
-popupVisible = ko.observable(false);
-showPopup = function () {
-  popupVisible(true);
-};
-hidePopup = function () {
-  popupVisible(false);
+
+    var askingFeedback = ko.observable(false);
+
+    var username = ko.observable('');
+    var password = ko.observable('');
+
+    function showStatus() {
+        DevExpress.ui.notify('Status: '+ askingFeedback(), 'info', 3000);
+    }
+
+    var viewModel = {
+
+        popup: {
+            visible: ko.observable(false),
+            showPopup: function() {
+                this.popup.visible(true);
+            },
+            hidePopup: function() {
+                this.popup.visible(false);
+                DevExpress.ui.notify('Name: '+ username()+' PW:'+password(), 'success', 3000);
+            }
+        },
+
+        askingFeedback: askingFeedback,
+        showStatus: showStatus,
+        username: username,
+        password: password
+
+
+    };
+    return viewModel;
 };
