170378ea592434d540b8d1c02252412366e7d97e
Kilian Gärtner <Bhaals@gmx.de>
2014-06-14 18:53:18 +0200

    Improve exercise view for teacher

--- views/exercise.js
+++ views/exercise.js
@@ -10,14 +10,21 @@ WeVote.exercise = function (params) {
         DevExpress.ui.notify('Exercise startet with ' + tasknumbers() + ' tasks', 'success', 2000);
     };
 
+    function incrementTaskCount() {
+        tasknumbers(tasknumbers()+1);
+    };
+
+    function decrementTaskCount() {
+        tasknumbers(tasknumbers()-1);
+    };
+
     function submit () {
-        DevExpress.ui.notify('Submited', 'success', 2000);
-        
+        DevExpress.ui.notify('Submited', 'success', 2000);        
     };
 
     function viewShown() {
-        $("#teacherview").toggle(GLOBAL_VARIABLES.isTeacher());
-        $("#studentview").toggle(!GLOBAL_VARIABLES.isTeacher());
+        $("#teacherview").toggle(!GLOBAL_VARIABLES.isTeacher());
+        $("#studentview").toggle(GLOBAL_VARIABLES.isTeacher());
     }
 
     var viewModel = {
@@ -28,6 +35,9 @@ WeVote.exercise = function (params) {
         checkedTask3: checkedTask3,
 
         startExercise: startExercise,
+        incrementTaskCount: incrementTaskCount,
+        decrementTaskCount: decrementTaskCount,
+
         submit: submit,
         viewShown: viewShown
 
