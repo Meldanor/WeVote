00cd636070f41ecc025c26f5532d71a228fa16c9
Kilian Gärtner <Bhaals@gmx.de>
2014-06-20 22:49:32 +0200

    Add missing variables...

--- data/GlobalVariables.js
+++ data/GlobalVariables.js
@@ -5,6 +5,10 @@ var GLOBAL_VARIABLES = {
 var isExerciseStarted = false;
 var numberOfExerciseTasks = 0;
 
+var navBarDisabled = ko.observable(true);
+
+var usernames = ["kilian", "julien", "haXXor1337"];
+var pwds      = ["melanie", "juliane", "qwerty"];
 
 function IsNumeric(input) {
     return (input - 0) == input && (''+input).replace(/^\s+|\s+$/g, "").length > 0;
