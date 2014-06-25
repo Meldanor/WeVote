ba9b74a6c6bc8eee59fd59285a2ee4460acc6e87
Kilian Gärtner <Bhaals@gmx.de>
2014-06-25 13:56:03 +0200

    Display the at least semester first

--- data/Lectures.js
+++ data/Lectures.js
@@ -1,14 +1,5 @@
 Lectures = [
-    {
-        key: '1. Semester',
-        items: [
-            { name: "EinfInf", voted: 20, possible: 30, day: 'Montag', time: '11:00', room: 'g29-336' },
-            { name: "TI I", voted: 10, possible: 13, day: 'Dienstag', time: '13:00', room: 'g29-114' },
-            { name: "Logik", voted: 25, possible: 33, day: 'Mittwoch', time: '13:00', room: 'g29-036' },
-            { name: "SchlüKo", voted: 0, possible: 0, day: 'Donnerstag', time: '13:00', room: 'g29-K36'  },
-            { name: "Mathe I", voted: 80, possible: 100, day: 'Freitag',time: '13:00',  room: 'g29-336'  }
-        ]
-    },
+    
     {
         key: '2. Semester',
         items: [
@@ -18,6 +9,16 @@ Lectures = [
             { name: "Mathe II", voted: 22, possible: 33,  day: 'Freitag',time: '13:00',  room: 'g29-336'  },
             { name: "SchlüKo II", voted: 0, possible: 0,  day: 'Freitag',time: '13:00',  room: 'g29-336'  },
             { name: "TrainSMK", voted: 0, possible: 0,  day: 'Freitag',time: '13:00',  room: 'g29-336'  }
+        ]
+    },
+    {
+        key: '1. Semester',
+        items: [
+            { name: "EinfInf", voted: 20, possible: 30, day: 'Montag', time: '11:00', room: 'g29-336' },
+            { name: "TI I", voted: 10, possible: 13, day: 'Dienstag', time: '13:00', room: 'g29-114' },
+            { name: "Logik", voted: 25, possible: 33, day: 'Mittwoch', time: '13:00', room: 'g29-036' },
+            { name: "SchlüKo", voted: 0, possible: 0, day: 'Donnerstag', time: '13:00', room: 'g29-K36'  },
+            { name: "Mathe I", voted: 80, possible: 100, day: 'Freitag',time: '13:00',  room: 'g29-336'  }
         ]
     }
 ];
